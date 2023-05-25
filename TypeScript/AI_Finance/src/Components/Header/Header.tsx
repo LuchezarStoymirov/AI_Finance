1d9b7930ea5747ed9db06b3d8ee2b315d49ea340
LuchezarStoymirov <luchostoimirov@gmail.com>
2023-05-25 21:34:11 +0300

    Add basic layout

---
+++ TypeScript/AI_Finance/src/Components/Header/Header.tsx
@@ -0,0 +1,37 @@
+import { Grid } from "@mui/material";
+import { Link } from "react-router-dom";
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <Grid container className="header-container">
+        <Grid item>
+          <img
+            src="src/images/CashGrab-logo.png"
+            alt="CashGrab logo"
+            className="logo-image"
+            style={{ width: "100%", maxWidth: "200px" }}
+          />
+        </Grid>
+        <Grid container justifyContent="flex-end" alignItems="center">
+          <Grid item className="header-link">
+            <Link to="/" className="header-link__item">
+              Home
+            </Link>
+          </Grid>
+          <Grid item className="header-link">
+            <Link to="/data" className="header-link__item">
+              Data
+            </Link>
+          </Grid>
+          <Grid item className="header-link">
+            <Link to="/profile" className="header-link__item">
+              Profile
+            </Link>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
---
+++ TypeScript/AI_Finance/src/Pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import './Home.css';
+
+export const Home = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1 className="title">Welcome to CashGrab.</h1>
+        <h3 className="description">
+          Our platform utilizes AI to provide information about current prices of selected Stocks and Companies.
+          The information given must not be taken as financial advice.
+        </h3>
+      </div>
+    </div>
+  );
+}
