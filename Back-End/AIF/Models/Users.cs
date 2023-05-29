a32633127f7d00920be47f6c7e25e70a035c2e8c
Ivaylo Petkov <131447184+IvayloPetkovBlankfactor@users.noreply.github.com>
2023-05-29 10:41:30 +0300

    Created the identities

---
+++ Back-End/AIF/Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace AIF.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
