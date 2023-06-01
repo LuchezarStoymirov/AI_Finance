92db1e5fcec2eafabb00604edc44eab6c17bb1da
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-06-01 13:14:35 +0300

    added error handling for fetch

--- TypeScript/AI_Finance/src/services/fetchWrapper.ts
+++ TypeScript/AI_Finance/src/services/fetchWrapper.ts
@@ -1,13 +1,9 @@
 import axios from "axios";
 
 const get = async (url: string) => {
-  try {
+  
     const response = await axios.get(url);
     return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
 };
 
 export const fetchWrapper = {
