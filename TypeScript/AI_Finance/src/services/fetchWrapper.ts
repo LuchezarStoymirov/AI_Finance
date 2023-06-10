7f27652c4126d02fcb15effbe886b682b7289541
LuchezarStoymirov <luchostoimirov@gmail.com>
2023-06-10 18:34:01 +0300

    add headers to get request

--- TypeScript/AI_Finance/src/services/fetchWrapper.ts
+++ TypeScript/AI_Finance/src/services/fetchWrapper.ts
@@ -3,9 +3,16 @@ import axios from "axios";
 
 
 const get = async (url: string) => {
-    const response = await axios.get(url);
-    return response.data;
+  const token = localStorage.getItem('token');
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+  const response = await axios.get(url, config);
+  return response.data;
 };
+
 
 const post = <T, D>(url:string, data: D) => {
   return axios.post<T>(url, data);
