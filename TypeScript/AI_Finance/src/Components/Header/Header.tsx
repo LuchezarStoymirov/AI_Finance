e3c2dd2f6090a0379a4cb436cbea1255e7031ae6
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-06-12 10:10:26 +0300

    Add profile drop down menu

--- TypeScript/AI_Finance/src/Components/Header/Header.tsx
+++ TypeScript/AI_Finance/src/Components/Header/Header.tsx
@@ -1,25 +1,68 @@
-import { Grid } from "@mui/material";
-import { Link } from "react-router-dom";
-import './Header.css';
+import { useState } from "react";
+import { Grid, IconButton, Menu, MenuItem } from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import "./Header.css";
 
 export const Header = () => {
+  const [auth, setAuth] = useState(true);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleChange = (event: any) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-      <Grid container className="header" alignItems="center">
-        <Grid item>
-          <img
-            src="src/images/CashGrab-logo-light.png"
-            alt="CashGrab logo"
-            className="logo-image"
-          />
-        </Grid>
-        <Grid item sx={{ marginLeft: 'auto' }}>
-          <Link to="/" className="header-link__item">
-            Home
-          </Link>
-          <Link to="/profile" className="header-link__item">
-            Profile
-          </Link>
-        </Grid>
+    <Grid container className="header" alignItems="center">
+      <Grid item>
+        <img
+          src="src/images/CashGrab-logo-light.png"
+          alt="CashGrab logo"
+          className="logo-image"
+        />
       </Grid>
+      <Grid item sx={{ marginLeft: "auto" }}>
+        {auth && (
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle sx={{ fontSize: 32, color: 'white' }} />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Username goes here</MenuItem>
+              <MenuItem onClick={handleClose}>User email goes here</MenuItem>
+              <MenuItem>Sign out</MenuItem>
+            </Menu>
+          </div>
+        )}
+      </Grid>
+    </Grid>
   );
 };
