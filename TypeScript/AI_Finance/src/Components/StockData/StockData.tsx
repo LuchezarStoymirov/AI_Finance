43ae2f441bf68a2a9fce9fd96481eab2ce88871e
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-06-21 14:28:21 +0300

    remove eslint ignore in stockData

--- TypeScript/AI_Finance/src/Components/StockData/StockData.tsx
+++ TypeScript/AI_Finance/src/Components/StockData/StockData.tsx
@@ -11,7 +11,6 @@ export const StockData = () => {
       try {
         setData(await apiService.getStockData());
       } catch (error) {
-        // eslint-disable-next-line no-console
         throw error;
       }
     })();
