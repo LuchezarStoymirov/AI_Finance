b692a5488126c65cca22893c5e7b76e6f8391662
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-06-22 12:21:11 +0300

    Add functionality to export button

--- TypeScript/AI_Finance/src/Config/urlConfig.ts
+++ TypeScript/AI_Finance/src/Config/urlConfig.ts
@@ -1,9 +1,9 @@
 export const config = {
-
-    baseURL: 'https://localhost:7085/api/',
-    register: 'register',
-    login: 'login',
-    authorise: 'user',
-    googleLogin: 'google-login',
-    scraping: 'Scraping'
-}
+  baseURL: "https://localhost:7085/api/",
+  register: "register",
+  login: "login",
+  authorise: "user",
+  googleLogin: "google-login",
+  scraping: "Scraping",
+  export: "Scraping/export/csv",
+};
