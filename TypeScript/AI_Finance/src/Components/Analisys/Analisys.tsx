033abe5ee412efd346da5692f250c5fcda6fd6d3
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-06-23 09:53:27 +0300

    commit

--- TypeScript/AI_Finance/src/Components/Analisys/Analisys.tsx
+++ TypeScript/AI_Finance/src/Components/Analisys/Analisys.tsx
@@ -1,3 +1,22 @@
 import style from "./Analisys.module.css";
 
-export const Analisys = () => <div className={style.info}></div>;
+export const Analisys = () => {
+  return (
+    <div className={style.info}>
+      <p className={style.content}>
+        ETH/USD ETH/USD 1,880.00-29.69 (-1.55%) - Real-time Data 1,884.99 20:00
+        22:00 23/06 1,870.00 1,880.00 1,890.00 Technical Summary 5 Min:Strong
+        Sell Hourly:Strong Buy Daily:Strong Buy Monthly:Buy What is your
+        sentiment on ETH/USD? or made an almost picture-perfect Fibonacci-based
+        impulse pattern from last week’s low where green waves 1, 3, and 5
+        topped and bottomed almost precisely where they ideally should (green
+        boxes). See Figure 1 below. Red W-ii/b should now be underway as the
+        price action since the June 17 high at $1760 is messy, i.e.,
+        overlapping, and thus corrective. Note green W-1 comprised five (grey)
+        waves as well, which also adhered well to the Fibonacci-based impulse
+        pattern. This is the first time we have seen it this good and evident in
+        a while.
+      </p>
+    </div>
+  );
+};
--- TypeScript/AI_Finance/src/Components/NewsRow/NewsRow.tsx
+++ TypeScript/AI_Finance/src/Components/NewsRow/NewsRow.tsx
@@ -1,20 +1,63 @@
-import style from './NewsRow.module.css'
+import style from "./NewsRow.module.css";
 
-export const NewsRow = (props: {title: string, content: string}) => {
-    return(
-        <div className={style.box}>
-            {/* <h3 className={style.title}>{props.title}</h3> */}
-            <h3></h3>
-            {/* <p>{props.content}</p> */}
-            <p style={{fontSize: '40px', color: 'yellow'}}>Wanted: Ruja Ignatova</p>
-            <img src="src/Images/download.jpeg" alt="" style={{borderRadius: '8px', border: '1px solid yellow'}}/>
-            <p>Framed for financial fraud by the haters, a woman cant make a living in a mans world, the misoginy is off the charts.
-                Ruja made it big in a mans world, but the patriarchy does not tolerate a honest days work if youre a woman.
-                "She a good gal" her mother stated. We believe her. Not only is she "guilty" for being born a woman, but she is subjected to racism becouse of her Bulgarian heritige. On unraleted news Boiko Borisov buys out 51% of CashGrab EOOD stock.
-            </p>
-            <p style={{fontSize: '40px', color: 'yellow'}}>CEO and Founder of CashGrab</p>
-            <img src="src/Images/download 2.jpeg" alt="" style={{borderRadius: '8px', border: '1px solid yellow'}}/>
-            <p>Roza Ignatova</p>
-        </div>
-    );
-}
+export const NewsRow = (props: { title: string; content: string }) => {
+  return (
+    <>
+      <div className={style.box}>
+        {/* <h3 className={style.title}>{props.title}</h3> */}
+        <h3></h3>
+        {/* <p>{props.content}</p> */}
+        <h3 className={style.title}>
+          Binance Labs and CoinFund Lead $10 Million Funding Round for Cosmos
+          Blockchain Neutron
+        </h3>
+        <img
+          src="src/Images/neutron.jpeg"
+          alt=""
+          style={{ width: "300px", borderRadius: "8px" }}
+        />
+        <p>
+          Binance Labs and CoinFund have co-led a $10 million funding round for
+          Neutron, a cross-chain smart contract platform focused on interchain
+          security within the Cosmos ecosystem. Binance Labs, the investment arm
+          of leading cryptocurrency exchange Binance, took the lead in the
+          funding round, Binance said in a Wednesday blog post. Cryptonative
+          investment firm CoinFund co-led the funding. Delphi Ventures,
+          LongHash, and Nomad also participated in the round. The funding will
+          enable the development and growth of Neutron's blockchain software and
+          promote growth for its ecosystem, Binance said in the announcement. It
+          added that the goal is to create "an environment that attracts a wider
+          community of developers and projects with the aim of creating
+          innovative, secure, and user-friendly decentralized applications
+          (DApps)."
+        </p>
+      </div>
+      <div className={style.box}>
+        {/* <h3 className={style.title}>{props.title}</h3> */}
+        <h3></h3>
+        {/* <p>{props.content}</p> */}
+        <h3 className={style.title}>
+          Binance Introduces Zero Maker Fees for TUSD Trading Pairs – The Next
+          Tether Stablecoin?
+        </h3>
+        <img
+          src="src/Images/binance.jpeg"
+          alt=""
+          style={{ width: "300px", borderRadius: "8px" }}
+        />
+        <p>
+          Crypto exchange giant Binance has announced a new zero-maker fee
+          promotion for stablecoin TUSD. Starting from 30th June, the
+          promotional offer covers both spot and margin trading for TrueUSD. The
+          zero trading fee promotion for USD stablecoin applies to all existing
+          and new USD stablecoin pairs on Binance spot and margin markets, as
+          per the announcement. However, the standard taker fees on these TUSD
+          spot and margin trading pairs will apply as usual. Maker and taker
+          fees offer a transaction rebate to traders who provide liquidity to
+          the market (maker) while charging traders who get that liquidity
+          (taker).
+        </p>
+      </div>
+    </>
+  );
+};
