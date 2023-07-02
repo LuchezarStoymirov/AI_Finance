ab2754e36c531bab278ddb87a2b00f31e99a2bb4
LuchezarStoymirov <luchostoimirov@gmail.com>
2023-07-02 22:37:09 +0300

    photo upload

--- TypeScript/AI_Finance/src/Pages/UserPanel/UserPanel.tsx
+++ TypeScript/AI_Finance/src/Pages/UserPanel/UserPanel.tsx
@@ -5,10 +5,10 @@ import { useEffect, useState } from "react";
 export const UserPanel = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      // eslint-disable-next-line no-useless-catch
       try {
         const res = await apiService.getUserData();
         setUsername(res.name);
@@ -20,11 +20,20 @@ export const UserPanel = () => {
     fetchData();
   }, []);
 
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setSelectedImage(file);
+    }
+  };
+
   return (
     <div className={style.container}>
       <div className={style.box}>
         <h3>{username}</h3>
         <h3>{email}</h3>
+        <input type="file" onChange={handleImageUpload} />
+        {selectedImage && <img src={URL.createObjectURL(selectedImage)} alt="Uploaded" className={style.profilePic}/>}
       </div>
     </div>
   );
